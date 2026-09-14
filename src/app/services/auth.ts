ff92807e53ea7e44dcf8921e89926328f8fc59cb
gabrielmsilva12-lang <gabriel.m.silva12@ba.estudante.senai.br>
2026-09-14 11:54:02 -0300

    first commit

---
+++ src/app/services/auth.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable }from '@angular/core';
+import { Usuario }from'../models/usuario.model';
+import {Observable}from'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  j
+  
+}
